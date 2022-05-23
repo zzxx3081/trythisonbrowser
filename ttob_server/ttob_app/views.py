8156c7b83e1fd6fb865d842df0d08c073bd5270d
Kang Minchul <tegongkang@gmail.com>
2022-05-24 02:23:12 +0900

    Initial frontend works finished
    This commit adds index, container, installation, login, register,
    and user settings in ttob

--- ttob_server/ttob_app/views.py
+++ ttob_server/ttob_app/views.py
@@ -2,3 +2,33 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def listimg(request):
+    return render(request, 'list.html')
+
+def userimg(request):
+    return render(request, 'user.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def container(request):
+    return render(request, 'container.html')
+
+def container_u(request):
+    return render(request, 'container_u.html')
+
+def container_c(request):
+    return render(request, 'container_c.html')
+
+def dockerfile(request):
+    return render(request, 'install_dockerfile.html')
+
+def script(request):
+    return render(request, 'install_script.html')
+
+def upgradepro(request):
+    return render(request, 'upgradepro.html')
--- ttob_server/ttob_server/urls.py
+++ ttob_server/ttob_server/urls.py
@@ -6,7 +6,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('list/', listimg, name="listimg"),
+    path('user/', userimg, name="userimg"),
+    path('login/', login, name="login"),
+    path('register/', register, name="register"),
+    path('container/gcc/latest/', container, name="container"),
+    path('container/ubuntu/18.04/', container_u, name="container_u"),
+    path('container/centos/7/', container_c, name="container_c"),
+    path('pro/', upgradepro, name="upgradepro"),
+    path('install/dockerfile', dockerfile, name="dockerfile"),
+    path('install/script', script, name="script"),
 ]
 
 if settings.DEBUG:
